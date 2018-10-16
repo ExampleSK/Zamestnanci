43e86b17f423490cde3c11ff07d4081a44e6a727
Matej Koutun <matej.koutun@t-mobile.cz>
2018-10-16 17:34:40 +0200

    Create solution

---
+++ ZamestnanciWeb/ZamestnanciWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZamestnanciWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
